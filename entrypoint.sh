a740f6d89ac4ef5230ad93598f99a56db417896a
ttuhg <63356932+ttuhg@users.noreply.github.com>
2022-05-30 13:29:43 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # Set port to start the app correctly
-sed -i "s/PORT/$PORT/g" /etc/nginx/http.d/default.conf
+sed -i "s/PORT/$PORT/g" /etc/nginx/default1.conf
 # Config tmal
 sed -i "s/UUID/$UUID/g" /etc/tmal/cf
 sed -i "s/V_PATH/$V_PATH/g" /etc/tmal/cf
-sed -i "s/V_PATH/$V_PATH/g" /etc/nginx/http.d/default.conf
+sed -i "s/V_PATH/$V_PATH/g" /etc/nginx/default1.conf
 
 wget https://raw.githubusercontent.com/ttuhg/tmal-heroku-1/master/index.html -O /var/lib/nginx/html/index.html
 REBOOTDATE=$(date)
@@ -14,6 +14,6 @@ sed -i "s/REBOOTDATE/$REBOOTDATE/g" /var/lib/nginx/html/index.html
 echo "The UUID is: $UUID, V_PATH is: $V_PATH,PORT is: $PORT"
 
 # start nginx
-nginx
+nginx -c /etc/nginx/default1.conf
 # main
 /usr/bin/supervisord -c /etc/supervisord.conf

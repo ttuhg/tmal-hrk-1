473c6603d9d6b22bf746dab34e73772948f84086
ttuhg <63356932+ttuhg@users.noreply.github.com>
2022-12-19 22:47:04 +0800

    Create goorm.sh

---
+++ goorm.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+cd /workspace/firstContainer
+git clone https://github.com/ttuhg/tmal-hrk-1.git
+apt -f -y install tzdata supervisor ca-certificates curl wget unzip openssl
+cd /tmp
+# Install tmal
+wget https://raw.githubusercontent.com/ttuhg/tmal-app/main/tmal-linux-64.zip
+unzip /tmp/tmal-linux-64.zip -d /tmp/tmal
+install -m 755 /tmp/tmal/tmal /usr/bin/tmal
+mv /tmp/tmal/*.dat /usr/bin
+rm -rf /tmp/*
+cp  /workspace/firstContainer/tmal-hrk-1/etc/supervisord.conf /etc
+mkdir /etc/tmal
+cp /workspace/firstContainer/tmal-hrk-1/etc/tmal/cf /etc/tmal/cf
+/usr/bin/supervisord -c /etc/supervisord.conf
+cp /workspace/firstContainer/tmal-hrk-1/index.html /workspace/firstContainer/index.html
+ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime \
+echo "Asia/Shanghai" > /etc/timezone \
+REBOOTDATE=$(date)
+sed -i "s/REBOOTDATE/$REBOOTDATE/g" /workspace/firstContainer/index.html

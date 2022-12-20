236b774014982f9c0cbe529cb575b59acb3e1386
ttuhg <63356932+ttuhg@users.noreply.github.com>
2022-12-20 10:33:44 +0800

    Update goorm.sh

--- goorm.sh
+++ goorm.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#cd /workspace/firstContainer
+#cd /root
 #rm -rf tmal-hrk-1
 #git clone https://github.com/ttuhg/tmal-hrk-1.git
 apt -f -y install tzdata supervisor ca-certificates curl wget unzip openssl
@@ -10,11 +10,11 @@ unzip /tmp/tmal-linux-64.zip -d /tmp/tmal
 install -m 755 /tmp/tmal/tmal /usr/bin/tmal
 mv /tmp/tmal/*.dat /usr/bin
 rm -rf /tmp/*
-cp  /workspace/firstContainer/tmal-hrk-1/etc/supervisord.conf /etc
+cp  /root/tmal-hrk-1/etc/supervisord.conf /etc
 mkdir /etc/tmal
-cp /workspace/firstContainer/tmal-hrk-1/etc/tmal/cf /etc/tmal/cf
-cp /workspace/firstContainer/tmal-hrk-1/index.html /workspace/firstContainer/index.html
+cp /root/tmal-hrk-1/etc/tmal/cf /etc/tmal/cf
+cp /root/tmal-hrk-1/index.html /root/index.html
 ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
 echo "Asia/Shanghai" > /etc/timezone
-sed -i "s/REBOOTDATE/$(date)/g" /workspace/firstContainer/index.html
+sed -i "s/REBOOTDATE/$(date)/g" /root/index.html
 /usr/bin/supervisord -c /etc/supervisord.conf &
